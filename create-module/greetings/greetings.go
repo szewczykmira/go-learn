fe0df53adb09c3ca1f6a9a4d59d4a3d8f4a3eefa
Mira <szewczyk.mira@gmail.com>
2021-02-23 10:54:22 +0100

    Create random func

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -3,13 +3,14 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // CalculateLength Return length of the name
 func CalculateLength(name string) int {
 	return len(name)
 }
-
 
 // Hello will return name and length of given name
 func Hello(name string) (string, error) {
@@ -19,5 +20,30 @@ func Hello(name string) (string, error) {
 
 	value := CalculateLength(name)
 	message := fmt.Sprintf("Hi, %v. Welcome! Do you know that your name has %d letters?", name, value)
+	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one value from the set of greetings
+func randomFormat() string {
+	formats := []string{
+		"Hi! %v welcome",
+		"Alloha %v",
+		"Dzien dobry %v!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
+
+// RandomGreetings will return random string
+func RandomGreetings(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Empty name was passed")
+	}
+
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
